9ae212d434459384fe16e109c4d4adbd6f7591d4
ankitkhani <contact.ankit89@gmail.com>
2014-09-14 22:18:28 -0700

    Initial Commit

---
+++ BestCPU/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  BestCPU
+//
+//  Created by Ankit Khani on 9/14/14.
+//  Copyright (c) 2014 CMPE. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
